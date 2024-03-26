9ab5eb144a505ec6bebbdbd0ed5b5217a28479bf
Stankovic Aleksandar <stankovic.sv25.2022@uns.ac.rs>
2024-03-26 10:51:47 +0100

    feat: added all templates

--- src/app/all-templates/all-templates.component.ts
+++ src/app/all-templates/all-templates.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AdminNavBarComponent } from '../admin-nav-bar/admin-nav-bar.component';
-
+//import  Template
 @Component({
   selector: 'app-all-templates',
   standalone: true,
@@ -9,5 +9,39 @@ import { AdminNavBarComponent } from '../admin-nav-bar/admin-nav-bar.component';
   styleUrl: './all-templates.component.css'
 })
 export class AllTemplatesComponent {
+  allTemplates: Template[] = [];
+  constructor(private modalService: NgbModal){}
+  
+  oepnDeleteTemplateModal(content: any, template: any){
+    this.modalService.open(content,{ariaLabelledBy: 'modal-basic-title'}).result.then(
+      () => {
+        // reset template
+        const idx = this.allTemplates.indexOf(template);
+        this.allTemplates.splice(idx,1);
+      },
+      () => {
+        // canceling
+        //nothing happens
+      }
+    );
+  }
 
+  openEditTemplateModal(content: any,template: any){
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+      () => {
+        // save input
+        const idx = this.allTemplates.indexOf(template);
+        this.allTemplates[idx] = template;
+      },
+      () => {
+        // canceling
+        //nothinghappens
+      }
+    );
+
+  }
+
+  openViewInputsModal(content:any,inputs:any){
+    //TODO VIEW INPUTS
+  }
 }
